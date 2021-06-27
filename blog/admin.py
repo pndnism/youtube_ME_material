809ad886a3df8d707351c17e9652ebe54c2dc138
pndnism <p.dai.man8@gmail.com>
2021-06-27 23:45:51 +0900

    feat: add VideoInfo model to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from blog.models import Category, Tag, Post
+from blog.models import Category, Tag, Post, VideoInfo
 
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Post)
+admin.site.register(VideoInfo)
